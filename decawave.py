9e3ced99fe2bedc4272bb373586fc7bc2b1185af
Trevor Overby <mr.blarg@gmail.com>
2019-03-23 09:50:46 -0600

    updated for glob vals and running ave.

--- decawave.py
+++ decawave.py
@@ -1,14 +1,14 @@
 import serial
 import time
+import re
 k = 2.0 # the number of samples in tau and diviser in the running average
-sample_rate = 10 #sample/second this is the sample rate of the decawave
-measurement_duration = k/sample_rate # sample/(sample/second) = seconds: time of taken measurements
-    
-def start_decawave():
-    k = 2.0 # the number of samples in tau and diviser in the running average
-    sample_rate = 10 #sample/second this is the sample rate of the decawave
-    measurement_duration = k/sample_rate # sample/(sample/second) = seconds: time of taken measurements
+counter = 0 #
+measurement_duration = 10*k # sample/(sample/second) = seconds: time of taken measurements
 
+def start_decawave():
+    global k # the number of samples in tau and diviser in the running average
+    global counter #
+    global measurement_duration # sample/(sample/second) = seconds: time of taken measurements
     running_ave = 0.0
 
     ser = serial.Serial('/dev/serial/by-id/usb-SEGGER_J-Link_000760044549-if00', 115200, timeout=None, xonxoff=True)
@@ -25,14 +25,15 @@ def start_decawave():
         
         result = ser.readline().split(b',')
         if result[0] == b'DIST':
-            temp = float(result[7].decode('utf-8'))
-            if running_ave > 0.0:
-                running_ave = running_ave - (1.0/k)*(running_ave-temp)
+            raw_data = (result[7].decode('utf-8'))
+            if re.fullmatch('[A-Za-z]', raw_data):
+                continue
             else:
-                running_ave = (1.0/k)*temp
-            print("raw data : ",(temp))
+                raw_data = float(raw_data)
+                running_ave = running_ave - (1.0/k)*(running_ave-raw_data)
+            print("raw data : ",(raw_data))
             print("running average : ", running_ave)
-        if start + 10< time.time():
+        if start + 10 < time.time(): #change to 400 samples
             ser.write(b'lec\r')
             time.sleep(1)
             ser.write(b'quit\r')
